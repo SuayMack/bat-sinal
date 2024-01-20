04b7879eb03c9f016edae33d5cd972d50dae8496
SuayMack <pry_chu@hotmail.com>
2024-01-20 18:41:28 -0300

    feat: criação do form de cadastro

---
+++ src/app/(tabs)/form.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import { Pressable, TextInput, View, Text, KeyboardAvoidingView, Platform, ScrollView } from 'react-native';
+
+import { styles } from '../Styles/formStyle';
+import { stylesButton } from '../../globalStyle/buttonStyle';
+import { Link } from 'expo-router';
+
+export default function Form() {
+
+  let [name, setName] = useState('');
+  let [email, setEmail] = useState('');
+  let [fone, setFone] = useState('');
+  let [location, setLocation] = useState('');
+  let [obs, setObs] = useState('');
+
+  function handleCadastrado() {
+    if (name === '' || email === '' || fone === '' || location === '') {
+      alert('Preencha todos os campos.');
+    } else {
+      alert('Cadastrado com sucesso');
+      setName(''),
+      setEmail(''),
+      setFone(''),
+      setLocation(''),
+      setObs('')
+    }
+  }
+
+  return (
+    <KeyboardAvoidingView behavior='padding' style={styles.container}>
+      <View style={styles.container}>
+        <TextInput style={styles.input} placeholder="Nome" placeholderTextColor="#e5c03cb3" value={name} onChangeText={setName} />
+        <TextInput style={styles.input} placeholder="E-mal" placeholderTextColor="#e5c03cb3" keyboardType='email-address' value={email} onChangeText={setEmail} />
+        <TextInput style={styles.input} placeholder="Fone" placeholderTextColor="#e5c03cb3" keyboardType='numeric' value={fone} onChangeText={setFone} />
+        <TextInput style={styles.input} placeholder="Localização" placeholderTextColor="#e5c03cb3" value={location} onChangeText={setLocation} />
+        <TextInput style={styles.input} placeholder="Observação" placeholderTextColor="#e5c03cb3" value={obs} onChangeText={setObs} />
+
+        <Text style={{ marginTop: 20 }}>
+          <Pressable onPress={handleCadastrado} style={stylesButton.button}>
+            <Text style={stylesButton.text}>⚡ REGISTRAR</Text>
+          </Pressable>
+        </Text>
+      </View>
+    </KeyboardAvoidingView>
+  );
+}
---
+++ src/app/Styles/formStyle.tsx
@@ -0,0 +1,25 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#E5BF3c',
+  },
+
+  input: {
+    justifyContent: 'center',
+    color: '#E5BF3c',
+    backgroundColor: '#181817',
+    alignSelf: 'center',
+    textAlign: 'center',
+    padding: 3,
+    fontSize: 20,
+    minWidth: '80%',
+    maxWidth: '99%',
+    height: 40,
+    borderRadius: 5,
+    margin:10
+  }  
+});
